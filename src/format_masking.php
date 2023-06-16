f8946c62cb22ab7a49a912cd8e8492d807cd6c7f
Ratinan Lee <lee.ratinan@gmail.com>
2023-06-16 22:50:09 +0800

    mask

--- src/format_masking.php
+++ src/format_masking.php
@@ -5,14 +5,9 @@
  * @param int $number_of_stars Number of stars
  * @return string
  */
-function format_add_stars (int $number_of_stars)
+function format_add_stars (int $number_of_stars): string
 {
-    $stars = '';
-    for ($i = 0; $i < $number_of_stars; $i++)
-    {
-        $stars .= '*';
-    }
-    return $stars;
+    return str_repeat('*', $number_of_stars);
 }
 
 /**
@@ -20,22 +15,22 @@ function format_add_stars (int $number_of_stars)
  * @param string $email The email address to be masked
  * @return string Masked email address, or empty string if the email is invalid
  */
-function format_mask_email (string $email)
+function format_mask_email (string $email): string
 {
     $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-    if (FALSE == $email)
+    if ( ! $email)
     {
         return '';
     }
     $explode = explode('@', $email);
-    $name = $explode[0];
-    $domain = $explode[1]; // MUST BE THERE BECAUSE $email IS ALREADY VALIDATED
-    $length_name = strlen($name);
-    $length_domain = strlen($domain);
-    $name_masked_length = floor($length_name * 0.5);
-    $name_unmasked_length = $length_name - $name_masked_length;
-    $domain_unmasked_length = floor($length_domain * 0.5);
-    $domain_masked_length = $length_domain - $domain_unmasked_length;
+    $name                   = $explode[0];
+    $domain                 = $explode[1]; // MUST BE THERE BECAUSE $email IS ALREADY VALIDATED
+    $length_name            = strlen($name);
+    $length_domain          = strlen($domain);
+    $name_masked_length     = floor($length_name * 0.5);
+    $name_unmasked_length   = $length_name - $name_masked_length;
+    $domain_unmasked_length = floor($length_domain * 0.6);
+    $domain_masked_length   = $length_domain - $domain_unmasked_length;
     return substr($name, 0, $name_unmasked_length) . format_add_stars($name_masked_length) . '@' . format_add_stars($domain_masked_length) . substr($domain, -$domain_unmasked_length);
 }
 
@@ -44,7 +39,7 @@ function format_mask_email (string $email)
  * @param string $card_number Credit card number in either '#### #### #### ####' or '################' format
  * @return string Masked credit card number, or empty string if the number is invalid
  */
-function format_mask_credit_card (string $card_number)
+function format_mask_credit_card (string $card_number): string
 {
     if (preg_match('/\d{4} \d{4} \d{4} \d{4}/', $card_number))
     {
@@ -61,7 +56,7 @@ function format_mask_credit_card (string $card_number)
  * @param string $input Any input
  * @return string Masked string, or empty string if the input is less than 5-character long
  */
-function format_mask_show_last_four (string $input)
+function format_mask_show_last_four (string $input): string
 {
     if (5 > strlen($input))
     {
