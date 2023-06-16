c1cf59d96e0ba39b5ec88fb63daea2ef08cb0fc0
Ratinan Lee <lee.ratinan@gmail.com>
2023-06-16 22:42:40 +0800

    distance

--- src/format_distance.php
+++ src/format_distance.php
@@ -6,14 +6,11 @@
  * @param string $from_unit Unit of the distance, either KM or MI
  * @return array Distances in both kilometers and miles, empty array if error
  */
-function convert_distance(float $from_distance, string $from_unit)
+function convert_distance(float $from_distance, string $from_unit): array
 {
     $from_unit = strtoupper($from_unit);
-    $from_distance = floatval($from_distance);
-    if (is_float($from_distance) && 0.0 < $from_distance)
+    if (0.0 <= $from_distance)
     {
-        $km = 0.0;
-        $mi = 0.0;
         if ('MI' == $from_unit)
         {
             $km = $from_distance * 1.609344;
@@ -24,15 +21,14 @@ function convert_distance(float $from_distance, string $from_unit)
             $mi = $from_distance / 1.609344;
         } else
         {
-            // $from_unit is invalid
             return [];
         }
         return [
             'KM' => $km,
             'MI' => $mi,
             'S' => [
-                'KM' => format_distance($km, 'KM', 'S'),
-                'MI' => format_distance($mi, 'MI', 'S')
+                'KM' => format_distance($km, 'KM'),
+                'MI' => format_distance($mi, 'MI')
             ],
             'L' => [
                 'KM' => format_distance($km, 'KM', 'L'),
@@ -51,9 +47,8 @@ function convert_distance(float $from_distance, string $from_unit)
  * @param string $mode The mode of the unit, either S (short) or L (long)
  * @return string The formatted distance, empty string if error
  */
-function format_distance(float $distance, string $unit, string $mode = 'S')
+function format_distance(float $distance, string $unit, string $mode = 'S'): string
 {
-    $distance = floatval($distance);
     $unit = strtoupper($unit);
     $mode = strtoupper($mode);
     $units = [
@@ -68,7 +63,7 @@ function format_distance(float $distance, string $unit, string $mode = 'S')
     ];
     if (isset($units[$unit][$mode]))
     {
-        return number_format($distance, 2, '.', ',') . $units[$unit][$mode];
+        return number_format($distance, 2) . $units[$unit][$mode];
     }
     return '';
 }
