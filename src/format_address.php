458b7f6005345b8e58efd71c3426ff1c95173694
Ratinan Lee <lee.ratinan@gmail.com>
2023-06-17 01:33:16 +0800

    address

--- src/format_address.php
+++ src/format_address.php
@@ -1,35 +1,75 @@
 <?php
 
 /**
- * Format the address into a fine Singapore format
- * There are 2 main formats: SHORT and FORMAL
- * SHORT:
- * | 123 Example Street, #12-34 Building Name, S(987654)
- * FORMAL:
- * | 123 Example Street
- * | #12-34 Building Name
- * | Singapore 987654
- * @param string $format the format of address, SHORT or FORMAL, default: FORMAL
- * @param string $street_address 123
- * @param string $street_name Example Street
- * @param string $postal_code 987654 (6-digit postal code)
- * @param string $unit_number (optional) #12-34, unit number usually starts with '#' sign
- * @param string $building_name (optional) The name of the building
+ * Format Singapore address, e.g.
+ * 123 Example Street
+ * #01-23 Building Name
+ * Singapore 123456
+ * @param string $street_number
+ * @param string $street_name
+ * @param string $postal_code
+ * @param string $floor (optional)
+ * @param string $unit_number (optional)
+ * @param string $building_name (optional)
  * @return string Formatted address
  */
-function format_address_singapore (string $format, string $street_address, string $street_name, string $postal_code, string $unit_number = '', string $building_name = '')
+function format_address_sg (string $street_number, string $street_name, string $postal_code, string $floor = '', string $unit_number = '', string $building_name = '') :string
 {
-    if (empty($street_address) || empty($street_name) || empty($postal_code))
+    // VALIDATE
+    if ( ! preg_match('/^\d+([A-Z])?$/', $street_number)
+        || empty($street_name)
+        || ! preg_match('/^\d{6}$/', $postal_code)
+        || ( ! empty($floor) && ! preg_match('/^([B]?\d+|(G|M))$/', $floor))
+        || ( ! empty($unit_number) && ! preg_match('/^\d+([A-Z])?$/', $unit_number)))
     {
-        return 'Missing street address or postal code';
+        return "";
     }
-    $line1 = trim("{$street_address} {$street_name}");
-    $line2 = trim("{$unit_number} {$building_name}");
-    if ('SHORT' == $format)
+    $street_name    = trim($street_name);
+    $building_name  = trim($building_name);
+    // FORMAT
+    $address_line_1 = "{$street_number} {$street_name}<br>";
+    $address_line_2 = "";
+    if ( ! empty($floor) && ! empty($unit_number))
     {
-        return $line1 . (empty($line2) ? ', S(' : ", {$line2}, S(") . $postal_code . ')';
-    } else
-    {
-        return $line1 . '<br>' . (empty($line2) ? '' : $line2 . '<br>') . "Singapore {$postal_code}";
+        $address_line_2 = trim("#{$floor}-{$unit_number} {$building_name}") . "<br>";
     }
+    $address_line_3 = "Singapore {$postal_code}";
+    return "{$address_line_1}{$address_line_2}{$address_line_3}";
+}
+
+/**
+ * Format USA address, e.g.
+ * 300 Main St.
+ * Apartment #3
+ * Cincinnati, OH 45202
+ * @param string $street_number
+ * @param string $street_name
+ * @param string $city_name
+ * @param string $state
+ * @param string $postal_code
+ * @param string $apartment_name (optional)
+ * @param string $unit_number (optional)
+ * @return string
+ */
+function format_address_us (string $street_number, string $street_name, string $city_name, string $state, string $postal_code, string $apartment_name = '', string $unit_number = ''): string
+{
+    $state = strtoupper($state);
+    if ( ! preg_match('/^[A-Z]{2}$/', $state)
+        || ! preg_match('/^\d{5}(\-\d{4})?$/', $postal_code))
+    {
+        return "";
+    }
+    $street_number  = trim($street_number);
+    $street_name    = trim($street_name);
+    $city_name      = trim($city_name);
+    $apartment_name = trim($apartment_name);
+    $unit_number    = trim($unit_number);
+    $address_line_1 = "{$street_number} {$street_name}<br>";
+    $address_line_2 = "";
+    if (! empty($apartment_name))
+    {
+        $address_line_2 = trim("{$apartment_name} {$unit_number}") . "<br>";
+    }
+    $address_line_3 = "{$city_name}, {$state} {$postal_code}<br>United States";
+    return "{$address_line_1}{$address_line_2}{$address_line_3}";
 }
--- test/address.php
+++ test/address.php
@@ -14,65 +14,100 @@
         $page = 'address';
         include_once '_nav.php';
         include_once '../src/format_address.php';
-        $func = filter_input(INPUT_POST, 'func', FILTER_SANITIZE_STRING);
-        $format = filter_input(INPUT_POST, 'format', FILTER_SANITIZE_STRING);
-        $street_address = filter_input(INPUT_POST, 'street_address', FILTER_SANITIZE_STRING);
-        $street_name = filter_input(INPUT_POST, 'street_name', FILTER_SANITIZE_STRING);
-        $unit_number = filter_input(INPUT_POST, 'unit_number', FILTER_SANITIZE_STRING);
+        $func          = filter_input(INPUT_POST, 'func', FILTER_SANITIZE_STRING);
+        $street_number = filter_input(INPUT_POST, 'street_number', FILTER_SANITIZE_STRING);
+        $street_name   = filter_input(INPUT_POST, 'street_name', FILTER_SANITIZE_STRING);
+        $postal_code   = filter_input(INPUT_POST, 'postal_code', FILTER_SANITIZE_STRING);
+        $city_name     = filter_input(INPUT_POST, 'city_name', FILTER_SANITIZE_STRING);
+        $floor         = filter_input(INPUT_POST, 'floor', FILTER_SANITIZE_STRING);
+        $unit_number   = filter_input(INPUT_POST, 'unit_number', FILTER_SANITIZE_STRING);
         $building_name = filter_input(INPUT_POST, 'building_name', FILTER_SANITIZE_STRING);
-        $postal_code = filter_input(INPUT_POST, 'postal_code', FILTER_SANITIZE_STRING);
+        $state         = filter_input(INPUT_POST, 'state', FILTER_SANITIZE_STRING);
         ?>
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <h1>Masking</h1>
-                    <h2>1. format_address_singapore()</h2>
+                    <h1>Address</h1>
+                    <!--- SG --->
+                    <h2>1. format_address_sg()</h2>
                     <p>
-                        Format the address into a fine Singapore format<br>
-                        There are 2 main formats: SHORT and FORMAL<br>
-                        SHORT:<br>
-                        | 123 Example Street, #12-34 Building Name, S(987654)<br>
-                        FORMAL:<br>
-                        | 123 Example Street<br>
-                        | #12-34 Building Name<br>
-                        | Singapore 987654<br>
-                        @param string $format the format of address, SHORT or FORMAL, default: FORMAL<br>
-                        @param string $street_address 123<br>
-                        @param string $street_name Example Street<br>
-                        @param string $postal_code 987654 (6-digit postal code)<br>
-                        @param string $unit_number (optional) #12-34, unit number usually starts with '#' sign<br>
-                        @param string $building_name (optional) The name of the building<br>
+                        Format Singapore address, e.g.<br>
+                        123 Example Street<br>
+                        #01-23 Building Name<br>
+                        Singapore 123456<br>
+                        @param string $street_number<br>
+                        @param string $street_name<br>
+                        @param string $postal_code<br>
+                        @param string $floor (optional)<br>
+                        @param string $unit_number (optional)<br>
+                        @param string $building_name (optional)<br>
                         @return string Formatted address
                     </p>
                     <h3>Test:</h3>
                     <form method="POST">
                         <div class="row row-cols-lg-auto g-3 align-items-center">
                             <div class="col">
-                                format_address_singapore(
+                                format_address_sg(
                             </div>
-                            <div class="col">
-                                <select class="form-control form-control-sm" name="format">
-                                    <option value="SHORT" <?= ('SHORT' == $format ? 'selected' : '') ?>>SHORT</option>
-                                    <option value="FORMAL" <?= ('FORMAL' == $format ? 'selected' : '') ?>>FORMAL</option>
-                                </select>
-                            </div>
-                            <div class="col">,</div>
-                            <div class="col"><input class="form-control form-control-sm" name="street_address" type="text" value="<?= $street_address ?>" placeholder="(street_address)" required /></div><div class="col">,</div>
+                            <div class="col"><input class="form-control form-control-sm" name="street_number" type="text" value="<?= $street_number ?>" placeholder="(street_number)" required /></div><div class="col">,</div>
                             <div class="col"><input class="form-control form-control-sm" name="street_name" type="text" value="<?= $street_name ?>" placeholder="(street_name)" required /></div><div class="col">,</div>
                             <div class="col"><input class="form-control form-control-sm" name="postal_code" type="text" value="<?= $postal_code ?>" placeholder="(postal_code)" required /></div><div class="col">,</div>
+                            <div class="col"><input class="form-control form-control-sm" name="floor" type="text" value="<?= $floor ?>" placeholder="(floor)" /></div><div class="col">,</div>
                             <div class="col"><input class="form-control form-control-sm" name="unit_number" type="text" value="<?= $unit_number ?>" placeholder="(unit_number)" /></div><div class="col">,</div>
                             <div class="col"><input class="form-control form-control-sm" name="building_name" type="text" value="<?= $building_name ?>" placeholder="(building_name)" /></div>
                             <div class="col">);</div>
-                            <input type="hidden" name="func" value="format_address_singapore" />
+                            <input type="hidden" name="func" value="format_address_sg" />
                             <div class="col"><input type="submit" class="btn btn-sm btn-success" value="Submit" /></div>
                         </div>
                     </form>
-                    <?php if ('format_address_singapore' == $func) : ?>
+                    <?php if ('format_address_sg' == $func) : ?>
                         <h3>Result</h3>
                         <p>Calls</p>
-                        <code>format_address_singapore('<?= $format ?>', '<?= $street_address ?>', '<?= $street_name ?>', '<?= $postal_code ?>', '<?= $unit_number ?>', '<?= $building_name ?>');</code>
+                        <code>format_address_sg('<?= $street_number ?>', '<?= $street_name ?>', '<?= $postal_code ?>', '<?= $floor ?>', '<?= $unit_number ?>', '<?= $building_name ?>');</code>
                         <p>Returns</p>
-                        <code><pre><?php print_r(format_address_singapore($format, $street_address, $street_name, $postal_code, $unit_number, $building_name)) ?></pre></code>
+                        <code><pre><?php print_r(format_address_sg($street_number, $street_name, $postal_code, $floor, $unit_number, $building_name)) ?></pre></code>
+                    <?php endif; ?>
+                    <hr>
+                    <!--- USA --->
+                    <h2>2. format_address_us()</h2>
+                    <p>
+                        Format USA address, e.g.<br>
+                        300 Main St.<br>
+                        Apartment #3<br>
+                        Cincinnati, OH 45202<br>
+                        @param string $street_number<br>
+                        @param string $street_name<br>
+                        @param string $city_name<br>
+                        @param string $state<br>
+                        @param string $postal_code<br>
+                        @param string $apartment_name (optional)<br>
+                        @param string $unit_number (optional)<br>
+                        @return string
+                    </p>
+                    <h3>Test:</h3>
+                    <form method="POST">
+                        <div class="row row-cols-lg-auto g-3 align-items-center">
+                            <div class="col">
+                                format_address_us(
+                            </div>
+                            <div class="col"><input class="form-control form-control-sm" name="street_number" type="text" value="<?= $street_number ?>" placeholder="(street_number)" required /></div><div class="col">,</div>
+                            <div class="col"><input class="form-control form-control-sm" name="street_name" type="text" value="<?= $street_name ?>" placeholder="(street_name)" required /></div><div class="col">,</div>
+                            <div class="col"><input class="form-control form-control-sm" name="city_name" type="text" value="<?= $city_name ?>" placeholder="(city_name)" required /></div><div class="col">,</div>
+                            <div class="col"><input class="form-control form-control-sm" name="state" type="text" value="<?= $state ?>" placeholder="(state)" required /></div><div class="col">,</div>
+                            <div class="col"><input class="form-control form-control-sm" name="postal_code" type="text" value="<?= $postal_code ?>" placeholder="(postal_code)" required /></div><div class="col">,</div>
+                            <div class="col"><input class="form-control form-control-sm" name="building_name" type="text" value="<?= $building_name ?>" placeholder="(apartment_name)" /></div>
+                            <div class="col"><input class="form-control form-control-sm" name="unit_number" type="text" value="<?= $unit_number ?>" placeholder="(unit_number)" /></div>
+                            <div class="col">);</div>
+                            <input type="hidden" name="func" value="format_address_us" />
+                            <div class="col"><input type="submit" class="btn btn-sm btn-success" value="Submit" /></div>
+                        </div>
+                    </form>
+                    <?php if ('format_address_us' == $func) : ?>
+                        <h3>Result</h3>
+                        <p>Calls</p>
+                        <code>format_address_us('<?= $street_number ?>', '<?= $street_name ?>', '<?= $city_name ?>', '<?= $state ?>', '<?= $postal_code ?>', '<?= $building_name ?>', '<?= $unit_number ?>');</code>
+                        <p>Returns</p>
+                        <code><pre><?php print_r(format_address_us($street_number, $street_name, $city_name, $state, $postal_code, $building_name, $unit_number)) ?></pre></code>
                     <?php endif; ?>
                     <hr>
                 </div>
