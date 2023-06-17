e44fd4f29da0246f838078ed9fbb17c75c9f28e9
Ratinan Lee <lee.ratinan@gmail.com>
2023-06-17 14:46:45 +0800

    date format - wip

--- src/format_date_time.php
+++ src/format_date_time.php
@@ -1,1 +1,103 @@
 <?php
+
+/**
+ * Retrieve the calendar array in various calendar
+ * @param string $string_date Date string in ISO8601 format, YYYY-MM-DD
+ * @param string $calendar Calendar, it could be GREGORIAN, JAPANESE, TAIWANESE, THAI
+ * @return array The array containing year, month, date, and era
+ */
+function retrieve_calendar_date (string $string_date, string $calendar): array
+{
+    if ( ! preg_match('/^\d{4}\-(0[1-9]|1[0-2])\-(0[1-9]|[1-2]\d|3[0-1])$/', $string_date)
+    || ! in_array($calendar, ['GREGORIAN', 'JAPANESE', 'TAIWANESE', 'THAI']))
+    {
+        return [];
+    }
+    $int_date = strtotime($string_date);
+    $year     = date('Y', $int_date);
+    $era      = '';
+    if (1971 > $year)
+    {
+        return [];
+    }
+    if ('THAI' == $calendar)
+    {
+        $year += 543;
+    } else if ('TAIWANESE' == $calendar)
+    {
+        $year -= 1911;
+    } else if ('JAPANESE' == $calendar)
+    {
+        if ('2019-05-01' <= $string_date)
+        {
+            $year -= 2018;
+            $era  = 'REIWA';
+        } else if ('1989-01-08' <= $string_date)
+        {
+            $year -= 1988;
+            $era  = 'HEISEI';
+        } else
+        {
+            $year -= 1925;
+            $era  = 'SHŌWA';
+        }
+    }
+    return [
+        'Y' => $year,
+        'M' => date('n', $int_date),
+        'D' => date('j', $int_date),
+        'E' => $era
+    ];
+}
+
+/**
+ * @param string $language_code
+ * @param string $format
+ * @return array The array containing the months in the specified language and format
+ */
+function retrieve_calendar_months (string $language_code, string $format): array
+{
+    $config = [
+        'DE' => [
+            'S' => ['', '', '', '', '', '', '', '', '', '', '', ''],
+            'L' => ['', '', '', '', '', '', '', '', '', '', '', '']
+        ],
+        'EN' => [
+            'S' => ['', '', '', '', '', '', '', '', '', '', '', ''],
+            'L' => ['', '', '', '', '', '', '', '', '', '', '', '']
+        ],
+        'ES' => [
+            'S' => ['', '', '', '', '', '', '', '', '', '', '', ''],
+            'L' => ['', '', '', '', '', '', '', '', '', '', '', '']
+        ],
+        'FR' => [
+            'S' => ['', '', '', '', '', '', '', '', '', '', '', ''],
+            'L' => ['', '', '', '', '', '', '', '', '', '', '', '']
+        ],
+        'IT' => [
+            'S' => ['', '', '', '', '', '', '', '', '', '', '', ''],
+            'L' => ['', '', '', '', '', '', '', '', '', '', '', '']
+        ],
+        'JP' => [
+            'S' => ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+            'L' => ['01月', '02月', '03月', '04月', '05月', '06月', '07月', '08月', '09月', '10月', '11月', '12月']
+        ],
+        'KO' => [
+            'S' => ['', '', '', '', '', '', '', '', '', '', '', ''],
+            'L' => ['', '', '', '', '', '', '', '', '', '', '', '']
+        ],
+        'TH' => [
+            'S' => ['ม.ค.', 'ก.พ.', 'มี.ค.', 'เม.ย.', 'พ.ค.', 'มิ.ย.', 'ก.ค.', 'ส.ค.', 'ก.ย.', 'ต.ค.', 'พ.ย.', 'ธ.ค.'],
+            'L' => ['มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน', 'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม']
+        ],
+        'ZH' => [
+            'S' => ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+            'L' => ['01月', '02月', '03月', '04月', '05月', '06月', '07月', '08月', '09月', '10月', '11月', '12月']
+        ]
+    ];
+    if (isset($config[$language_code][$format]))
+    {
+        return $config[$language_code][$format];
+    }
+    return [];
+}
--- test/date_time.php
+++ test/date_time.php
@@ -14,12 +14,52 @@
         $page = 'date-time';
         include_once '_nav.php';
         include_once '../src/format_date_time.php';
+        $func        = filter_input(INPUT_POST, 'func', FILTER_SANITIZE_STRING);
+        $string_date = filter_input(INPUT_POST, 'string_date', FILTER_SANITIZE_STRING);
+        $calendar    = filter_input(INPUT_POST, 'calendar', FILTER_SANITIZE_STRING);
         ?>
         <div class="container mb-3">
             <div class="row">
                 <div class="col">
                     <h1>Date-Time</h1>
-                    <h2 id="">1. ___()</h2>
+                    <h2 id="retrieve_calendar_date">1. retrieve_calendar_date()</h2>
+                    <p>
+                        Retrieve the calendar array in various calendar<br>
+                        @param string $string_date Date string in ISO8601 format, YYYY-MM-DD<br>
+                        @param string $calendar Calendar, it could be GREGORIAN, JAPANESE, TAIWANESE, THAI<br>
+                        @return array The array containing year, month, date, and era
+                    </p>
+                    <h3>Test:</h3>
+                    <form method="POST" action="date_time.php#retrieve_calendar_date">
+                        <div class="row row-cols-lg-auto g-3 align-items-center">
+                            <div class="col">
+                                retrieve_calendar_date(
+                            </div>
+                            <div class="col">
+                                <input class="form-control form-control-sm" name="string_date" type="text" value="<?= $string_date ?>" placeholder="(date)" required />
+                            </div>
+                            <div class="col">,</div>
+                            <div class="col">
+                                <select name="calendar" class="form-control form-control-sm" required>
+                                    <option disabled>(calendar</option>
+                                    <option value="GREGORIAN" <?= ('GREGORIAN' == $calendar ? 'selected' : '') ?>>Gregorian</option>
+                                    <option value="JAPANESE" <?= ('JAPANESE' == $calendar ? 'selected' : '') ?>>Japanese</option>
+                                    <option value="TAIWANESE" <?= ('TAIWANESE' == $calendar ? 'selected' : '') ?>>Taiwanese</option>
+                                    <option value="THAI" <?= ('THAI' == $calendar ? 'selected' : '') ?>>Thai</option>
+                                </select>
+                            </div>
+                            <div class="col">);</div>
+                            <input type="hidden" name="func" value="retrieve_calendar_date" />
+                            <div class="col"><input type="submit" class="btn btn-sm btn-success" value="Submit" /></div>
+                        </div>
+                    </form>
+                    <?php if ('retrieve_calendar_date' == $func) : ?>
+                        <h3>Result</h3>
+                        <p>Calls</p>
+                        <code>retrieve_calendar_date(<?= $string_date ?>, '<?= $calendar ?>');</code>
+                        <p>Returns</p>
+                        <code><pre><?php print_r(retrieve_calendar_date($string_date, $calendar)) ?></pre></code>
+                    <?php endif; ?>
                     <hr>
                 </div>
             </div>
